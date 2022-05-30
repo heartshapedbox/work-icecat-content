7e657eac888f85e8d5bb5449b2d1116cc40f0079
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-05-30 16:51:30 +0300

    Commmit

--- get_products.js
+++ get_products.js
@@ -1,13 +1,19 @@
 // get products attribute [MPN] from "push me to see my products" overlay-baloon target-element from the path Defition > Search > Feature > Values
+document.getElementsByClassName('divajax')[0].click();
 
-const container = document.getElementsByClassName("maintxt")[0];
-const element = container.querySelectorAll("a");
-const products = [];
-
-function getProducts() {
+function getMPN() {
+  const container = document.getElementsByClassName("maintxt")[0];
+  const element = container.querySelectorAll("a");
+  const products = [];
   for (var i = 0; i < element.length; i++) {
     products.push(element[i].innerHTML);
   };
   console.log(products.join("\n"));
 };
-getProducts();
+
+
+function getData() {
+  setTimeout(getMPN, 10000);
+}
+
+getData();
