f39d0b0c6b107a129a3eb5b12a368dfd22bdd942
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-05-31 12:07:37 +0300

    Commit

---
+++ get_product_ID.js
@@ -0,0 +1,16 @@
+// get product id from Product > Search page
+
+const container = document.getElementsByClassName('product-row');
+const prodAttributes = {}
+
+function getProductID() {
+  for (var i = 0; i < container.length; i++) {
+    const element = container[i].querySelectorAll('a')[0];
+    const prodMPN = element.innerHTML;
+    const prodID = element.getAttribute("href").split('=')[2].split(';')[0];
+    prodAttributes[`MPN: ${prodMPN}`] = `ID: ${prodID}`
+  };
+  console.log(prodAttributes)
+};
+
+getProductID()
